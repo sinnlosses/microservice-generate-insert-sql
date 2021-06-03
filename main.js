912fea0844e4b8a72c9bd9970bd021a4b6262f9e
fuji <fujiyoshiyuki@gmail.com>
2021-06-03 14:59:09 +0900

    完了

--- main.js
+++ main.js
@@ -2,44 +2,55 @@
  * 処理を実行する.
  */
 function execute() {
-    var inputText = getHtmlInputElementById("inputTextarea").value.trim();
+    var inputText = getHtmlInputElementById("inputTextarea").value;
     var inputRows = inputText.split("\n");
-    var tableName = getHtmlInputElementById("tableName").value;
-    var keyColumn = getHtmlInputElementById("keyColumn").value;
+    var alphabets = new Alphabets();
+    var shiftingDate = new ShiftingDate();
     var outputRows = new Array(inputRows.length);
     for (var i = 0; i < inputRows.length; i++) {
-        var splitedRows = identifySplitCharacter(inputRows[i]);
+        var splitedRows = split(inputRows[i]);
         var englishName = splitedRows[0].trim();
         var typeName = splitedRows[1].trim();
+        // dbkeyは自動採番のため不要
+        if (englishName.toLowerCase() == "dbkey") {
+            outputRows[i] = "";
+            continue;
+        }
+        if (typeName == "datetime") {
+            outputRows[i] = shiftingDate.getOne();
+            continue;
+        }
+        // 日付には長さが定義されていないため数値変換できない
+        // そのためここで初めて長さと精度を数値化する
         var length_1 = Number(splitedRows[2].trim());
-        var keyColumnValue = splitedRows[3].trim();
-        //データ定義が数値型、日付型の場合は処理しない
-        if (["int", "numeric", "datetime"].includes(typeName)) {
+        var precision = isNullToZero(splitedRows[3].trim());
+        //データ定義が数値型の場合は1〜9の数字を指定の長さになるまで繰り返してデータとする
+        if (["int", "numeric", "bigint"].includes(typeName.toLowerCase())) {
+            var nums = "123456789";
+            var q = length_1 / nums.length;
+            var r = length_1 % nums.length;
+            var numberData = nums.repeat(q) + nums.substring(0, r);
+            if (precision != 0) {
+                numberData = numberData + "." + nums.substring(0, precision);
+            }
+            outputRows[i] = numberData;
             continue;
         }
-        //長さの定義がない場合と1の場合は飛ばす
-        else if ([" ", "", "1"].includes(length_1.toString())) {
-            continue;
-        }
-        //長さの定義が8以上の場合、改行の制御コードを入れたデータをSETするUPDATE文を作成する
-        else if (length_1 >= 8) {
-            outputRows[i] = "UPDATE " + tableName + "\nSET " + englishName + " = 'sei' + CHAR(13) + CHAR(10) + 'gyo' " + "\nWHERE " + keyColumn + " = " + keyColumnValue;
-            continue;
-        }
-        //長さの定義が8より小さい場合、タブの制御コードを入れたデータをSETするUPDATE文を作成する
-        else if (length_1 > 2 && length_1 < 8) {
-            outputRows[i] = "UPDATE " + tableName + "\nSET " + englishName + " = 's' + CHAR(9) + 'g' " + "\nWHERE " + keyColumn + " = " + keyColumnValue;
-            continue;
-        }
-        else if (length_1 == 2) {
-            outputRows[i] = "UPDATE " + tableName + "\nSET " + englishName + " = CHAR(9) + 's' " + "\nWHERE " + keyColumn + " = " + keyColumnValue;
+        //データ定義が文字列の場合
+        if (["char", "varchar"].includes(typeName.toLowerCase())) {
+            if (length_1 == 1) {
+                outputRows[i] = alphabets.getOne();
+            }
+            else if (length_1 <= englishName.length) {
+                outputRows[i] = englishName.substring(0, length_1);
+            }
+            else {
+                outputRows[i] = addHyphen(englishName, length_1);
+            }
         }
     }
-    outputRows = outputRows.filter(function (value) {
-        return value != "";
-    });
     var outputTextarea = document.getElementById("outputTextarea");
-    outputTextarea.value = outputRows.join("\n\n");
+    outputTextarea.value = outputRows.join("\t");
 }
 /**
  * 指定したIDを持つエレメントを返す.
@@ -48,7 +59,12 @@ function execute() {
 function getHtmlInputElementById(id) {
     return document.getElementById(id);
 }
-function identifySplitCharacter(inputRow) {
+/**
+ * 行データを特定の文字で分割する.
+ * @param inputRow 行データ
+ * @returns 分割済みデータ
+ */
+function split(inputRow) {
     if (inputRow.split("\t").length >= 2) {
         return inputRow.split("\t");
     }
@@ -56,3 +72,107 @@ function identifySplitCharacter(inputRow) {
         return inputRow.split(" ");
     }
 }
+/**
+ * 精度を数値化する. 精度が未入力なら0を返す.
+ * @param precision 精度
+ * @returns 精度を数値化したもの
+ */
+function isNullToZero(precision) {
+    if (precision == "") {
+        return 0;
+    }
+    return Number(precision);
+}
+function addHyphen(englishName, columnLength) {
+    var output = englishName;
+    for (var i = englishName.length + 1; i <= columnLength; i++) {
+        if (i % 10 == 0) {
+            output = output + i / 10;
+        }
+        else if (i == columnLength) {
+            output = output + columnLength % 10;
+        }
+        else {
+            output = output + "-";
+        }
+    }
+    return output;
+}
+var Alphabets = /** @class */ (function () {
+    /**
+     * コンストラクタ
+     * アルファベットの初期化
+     * カウンタの初期化
+     */
+    function Alphabets() {
+        this.alphabets = this.getAlphabets();
+        this.count = 0;
+    }
+    /**
+     * アルファベット26文字をA, B, ..., Zの順で取得する.
+     * @returns アルファベット
+     */
+    Alphabets.prototype.getAlphabets = function () {
+        var c = 'a'.charCodeAt(0);
+        var alphabets = Array.apply(null, new Array(26)).map(function (v, i) {
+            return String.fromCharCode(c + i);
+        });
+        return alphabets;
+    };
+    /**
+     * カウンタのインデックスに対応するアルファベット1文字を取得する
+     * @returns
+     */
+    Alphabets.prototype.getOne = function () {
+        var ret = this.alphabets[this.count];
+        this.upCount();
+        return ret;
+    };
+    /**
+ * カウントアップ.
+     */
+    Alphabets.prototype.upCount = function () {
+        this.count++;
+        if (this.count >= 26) {
+            this.count = 0;
+        }
+    };
+    return Alphabets;
+}());
+var ShiftingDate = /** @class */ (function () {
+    /**
+     * コンストラクタ
+     */
+    function ShiftingDate() {
+        this.date = new Date();
+        this.count = 0;
+    }
+    /**
+     * カウンタのインデックス分日付を前にずらした日付を取得する
+     * @returns
+     */
+    ShiftingDate.prototype.getOne = function () {
+        this.date.setDate(this.date.getDate() - this.count);
+        this.upCount();
+        return this.toString(this.date);
+    };
+    /**
+     * 日付を文字列に変換する.
+     * @param dt 日付
+     * @returns 日付の文字列
+     */
+    ShiftingDate.prototype.toString = function (dt) {
+        var y = dt.getFullYear();
+        var m = ("00" + (dt.getMonth() + 1)).slice(-2);
+        var d = ("00" + dt.getDate()).slice(-2);
+        var result = y + "-" + m + "-" + d;
+        return result;
+    };
+    /**
+ * カウントアップ.
+     */
+    ShiftingDate.prototype.upCount = function () {
+        this.count++;
+    };
+    return ShiftingDate;
+}());
--- main.ts
+++ main.ts
@@ -2,59 +2,60 @@
  * 処理を実行する.
  */
  function execute():void{
-    const inputText:string = getHtmlInputElementById("inputTextarea").value.trim();
+    const inputText:string = getHtmlInputElementById("inputTextarea").value;
     const inputRows:string[] = inputText.split("\n");
-    const tableName:string = getHtmlInputElementById("tableName").value;
-    const now:string = new Date().toDateString();
+    const alphabets = new Alphabets();
+    const shiftingDate = new ShiftingDate();
     let outputRows:string[] = new Array(inputRows.length);
 
     for (let i = 0; i < inputRows.length; i++){
-        const splitedRows:string[] = identifySplitCharacter(inputRows[i]);
+        const splitedRows:string[] = split(inputRows[i]);
         const englishName:string = splitedRows[0].trim();
         const typeName:string = splitedRows[1].trim();
-        const length:number = Number(splitedRows[2].trim());
-        const keyColumnValue:String = splitedRows[3].trim();
 
-        //データ項目がDBKEYの場合
-        if(englishName == "DBKEY"){
+        // dbkeyは自動採番のため不要
+        if(englishName.toLowerCase() == "dbkey"){
+            outputRows[i] = "";
             continue
         }
-        //データ定義が数値型の場合は1〜9の数字をしての長さになるまで繰り返してデータとする
-        //小数点ありのデータまた今度
-        else if(["int" , "numeric"].includes(typeName)){
-            let numberData:string = "";
-            for(let j = 1; j = length; j++){
-                numberData = numberData + j.toString()
+        if(typeName == "datetime"){
+            outputRows[i] = shiftingDate.getOne();
+            continue
+        }
+
+        // 日付には長さが定義されていないため数値変換できない
+        // そのためここで初めて長さと精度を数値化する
+        const length:number = Number(splitedRows[2].trim());
+        const precision:number = isNullToZero(splitedRows[3].trim());
+
+        //データ定義が数値型の場合は1〜9の数字を指定の長さになるまで繰り返してデータとする
+        if(["int" , "numeric", "bigint"].includes(typeName.toLowerCase())){
+            const nums: string = "123456789";
+            const q = length/nums.length;
+            const r = length%nums.length;
+            let numberData:string = nums.repeat(q) + nums.substring(0, r);
+            if (precision != 0){
+                numberData = numberData + "." + nums.substring(0, precision)
             }
             outputRows[i] = numberData;
             continue
         }
-        //データ定義が日付型の場合は現在時刻をデータとする
-        else if(typeName == "datetime"){
-            outputRows[i] = now;
-            continue
-        }
         //データ定義が文字列の場合
-        //長さの定義が1の場合
-        //長さの定義が2以上かつ10より短い場合
-        
-        else if(length >= 8){
-            continue
-        }
-        //長さの定義が8より小さい場合、タブの制御コードを入れたデータをSETするUPDATE文を作成する
-        else if (length > 2 && length < 8){
-            continue
-        }
-        else if(length == 2){
+        if(["char" , "varchar"].includes(typeName.toLowerCase())){
+            if(length == 1){
+                outputRows[i] = alphabets.getOne();
+            }
+            else if(length <= englishName.length){
+                outputRows[i] = englishName.substring(0, length);
+            }
+            else{
+                outputRows[i] = addHyphen(englishName, length);
+            }
         }
     }
-    
-    outputRows = outputRows.filter(function(value){
-        return value != "";
-    });
 
     let outputTextarea:HTMLInputElement = <HTMLInputElement>document.getElementById("outputTextarea");
-    outputTextarea.value = outputRows.join("\n\n");
+    outputTextarea.value = outputRows.join("\t");
 }
 
 /**
@@ -65,10 +66,131 @@ function getHtmlInputElementById(id:string):HTMLInputElement{
     return <HTMLInputElement>document.getElementById(id);
 }
 
-function identifySplitCharacter(inputRow:string):string[]{
+/**
+ * 行データを特定の文字で分割する.
+ * @param inputRow 行データ
+ * @returns 分割済みデータ
+ */
+function split(inputRow:string):string[]{
     if (inputRow.split("\t").length >= 2){
         return inputRow.split("\t");
     } else {
         return inputRow.split(" ");
+    }
+}
+
+/**
+ * 精度を数値化する. 精度が未入力なら0を返す.
+ * @param precision 精度
+ * @returns 精度を数値化したもの
+ */
+function isNullToZero(precision:string):number{
+    if (precision == ""){
+        return 0;
+    }
+    return Number(precision);
+}
+
+function addHyphen(englishName: string, columnLength: number):string{
+    let output:string = englishName;
+    for(let i:number=englishName.length+1; i <= columnLength; i++){
+        if (i % 10 == 0){
+            output = output + i/10;
+        } else if (i == columnLength){
+            output = output + columnLength%10;
+        } else {
+            output = output + "-";
+        }
+    }
+    return output;
+}
+
+class Alphabets {
+    alphabets: string[];
+    count: number;
+
+    /**
+     * コンストラクタ
+     * アルファベットの初期化
+     * カウンタの初期化
+     */
+    constructor() {
+        this.alphabets = this.getAlphabets();
+        this.count = 0;
+    }
+
+    /**
+     * アルファベット26文字をA, B, ..., Zの順で取得する.
+     * @returns アルファベット
+     */
+    getAlphabets():string[]{
+        const c = 'a'.charCodeAt(0);
+        const alphabets = Array.apply(null, new Array(26)).map((v: any, i: number) => {
+        return String.fromCharCode(c + i);
+        });
+        return alphabets;
+    }
+
+    /**
+     * カウンタのインデックスに対応するアルファベット1文字を取得する
+     * @returns 
+     */
+    getOne(): string{
+        const ret:string = this.alphabets[this.count];
+        this.upCount();
+        return ret;
+    }
+
+    /**
+ * カウントアップ.
+     */
+    upCount():void{
+        this.count++;
+        if (this.count >= 26){
+            this.count = 0;
+        }
+    }
+}
+
+class ShiftingDate {
+    date: Date;
+    count: number;
+
+    /**
+     * コンストラクタ
+     */
+    constructor() {
+        this.date = new Date();
+        this.count = 0;
+    }
+
+    /**
+     * カウンタのインデックス分日付を前にずらした日付を取得する
+     * @returns 
+     */
+    getOne(): string{
+        this.date.setDate(this.date.getDate() - this.count);
+        this.upCount();
+        return this.toString(this.date);
+    }
+
+    /**
+     * 日付を文字列に変換する.
+     * @param dt 日付
+     * @returns 日付の文字列
+     */
+    toString(dt:Date){
+        var y = dt.getFullYear();
+        var m = ("00" + (dt.getMonth()+1)).slice(-2);
+        var d = ("00" + dt.getDate()).slice(-2);
+        var result = y + "-" + m + "-" + d;
+        return result;
+    }
+
+    /**
+ * カウントアップ.
+     */
+    upCount():void{
+        this.count++;
     }
 }
